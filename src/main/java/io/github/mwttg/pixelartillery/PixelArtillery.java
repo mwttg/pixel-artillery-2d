8643ac2c8b6639ab5d793e03a14cfc117decbedb
Marco Wittig <marco-wittig@gmx.de>
2024-01-22 21:38:17 +0100

    Add OpenGL configuration class

--- src/main/java/io/github/mwttg/pixelartillery/PixelArtillery.java
+++ src/main/java/io/github/mwttg/pixelartillery/PixelArtillery.java
@@ -1,5 +1,6 @@
 package io.github.mwttg.pixelartillery;
 
+import io.github.mwttg.pixelartillery.config.OpenGlConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,6 +9,8 @@ public class PixelArtillery {
     private static final Logger LOG = LoggerFactory.getLogger(PixelArtillery.class);
 
     public static void main(String[] args) {
+        final var configuration = new OpenGlConfiguration("Test", 800, 600, true, true, 0.01f, 20.0f);
+        LOG.info(configuration.prettyFormat());
         LOG.info("Hello World");
     }
 }
---
+++ src/main/java/io/github/mwttg/pixelartillery/config/OpenGlConfiguration.java
@@ -0,0 +1,24 @@
+package io.github.mwttg.pixelartillery.config;
+
+public record OpenGlConfiguration(
+        String title,
+        int width,
+        int height,
+        boolean fullScreen,
+        boolean vSync,
+        float nearPlane,
+        float farPlane
+) {
+
+    public String prettyFormat() {
+        return """
+                OpenGL 4.1 Configuration
+                    - Title ......................... %s
+                    - Window Dimension .............. %sx%s
+                    - FullScreen .................... %s
+                    - VSync ......................... %s
+                    - near Plane .................... %.2f
+                    - far Plane ..................... %.2f
+                """.formatted(title, width, height, fullScreen, vSync, nearPlane, farPlane);
+    }
+}
