1e791262270b4f0247c70152c95447549555fe5c
Marco Wittig <marco-wittig@gmx.de>
2024-01-22 18:54:48 +0100

    Add basic dependencies

---
+++ src/main/java/io/github/mwttg/pixelartillery/PixelArtillery.java
@@ -0,0 +1,13 @@
+package io.github.mwttg.pixelartillery;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PixelArtillery {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PixelArtillery.class);
+
+    public static void main(String[] args) {
+        LOG.info("Hello World");
+    }
+}
