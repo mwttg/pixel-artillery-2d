bfe7fb19f61cde6173ee82feab24e69db078749e
Marco Wittig <marco-wittig@gmx.de>
2024-01-25 19:02:19 +0100

    Add simple abstract game loop

---
+++ src/main/java/io/github/mwttg/pixelartillery2d/mainloop/AbstractMainLoop.java
@@ -0,0 +1,20 @@
+package io.github.mwttg.pixelartillery2d.mainloop;
+
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.opengl.GL41;
+
+public abstract class AbstractMainLoop {
+
+    protected abstract void execute();
+
+    public void loop(final long gameWindowId) {
+        while (!GLFW.glfwWindowShouldClose(gameWindowId)) {
+            GL41.glClear(GL41.GL_COLOR_BUFFER_BIT | GL41.GL_DEPTH_BUFFER_BIT);
+
+            execute();
+
+            GLFW.glfwSwapBuffers(gameWindowId);
+            GLFW.glfwPollEvents();
+        }
+    }
+}
